2b5cbc728200b74b7dcf6204a0c85912defe2ee9
ValentinAmani <valentin01amani@gmail.com>
2023-11-24 22:14:05 +0000

    add back production host

--- front/src/api/host.js
+++ front/src/api/host.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:8080/api";
-// const host = ""
+// const host = "http://localhost:8080/api";
+const host = "https://track-list-back.vercel.app/api"
 
 export default host;
